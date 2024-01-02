e39e0a60a55db55957769afa7b8bef82787d6a96
Abhijit Barman <113384779+Abhijit0Barman@users.noreply.github.com>
2024-01-02 20:06:52 +0530

    csd

--- frontend/src/features/cart/Cart.js
+++ frontend/src/features/cart/Cart.js
@@ -97,6 +97,7 @@ export default function Cart() {
         </div>
       </div>
 
+
       <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
         <div className="flex justify-between text-base font-medium text-gray-900">
           <p>Subtotal</p>
